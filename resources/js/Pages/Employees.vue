90583803a625b762ca03a62843815bc653667019
Víctor J. <maximovj@outlook.com>
2025-04-10 08:55:20 -0600

    feature: Se crea nueva ruta

---
+++ resources/js/Pages/Employees.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+import { useAuthCache } from '@/composables/useAuthCache';
+
+const { hasRole, hasPermission } = useAuthCache();
+
+const user = computed(() => usePage().props.auth.user);
+const currentRoles = computed(() => user.value?.roles || []);
+const currentPermissions = computed(() => user.value?.permissions || []);
+</script>
+
+<template>
+    <Head title="Employees" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Employees</h2>
+        </template>
+
+        <div  class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">Show lists employees</div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/employees', function () {
+    return Inertia::render('Employees');
+})->middleware(['auth', 'verified'])->name('employees');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
