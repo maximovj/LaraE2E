5c93de4621fc19950f7ab949a208ea6aebf48e76
Víctor J. <maximovj@outlook.com>
2025-04-10 08:54:55 -0600

    feature: Se crea modelo y migración para empleados

---
+++ app/Models/Employee.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    // Especificamos la tabla
+    protected $table = 'employees';
+
+    // Especificamos el campo primario
+    protected $primaryKey = 'id';
+
+    // Especificamos el tipo de la clave primaria
+    protected $keyType = 'int';
+
+    // Especificamos los atributos que no pueden ser asignados masivamente
+    protected $guarded = ['id'];
+
+    // Especificamos los atributos que pueden ser asignados masivamente
+    protected $fillable = [
+        'user_id',
+        'company_id',
+        'office_id',
+        'manager_id',
+        'employee_number',
+        'job_title',
+        'position',
+        'hired_at',
+        'status',
+        'salary',
+        'shift',
+        'emergency_contact',
+    ];
+
+    // Especificamos los campos de tipo fecha que deben ser convertidos a instancias Carbon
+    protected $dates = [
+        'hired_at',
+        'created_at',
+        'updated_at',
+        'deleted_at',  // Campo de soft delete
+    ];
+
+    // Especificamos cómo los atributos deben ser casteados
+    protected $casts = [
+        'salary' => 'decimal:2', // Casteo del salario a decimal
+    ];
+
+    // ******************************************************
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>> RELACIONES
+    // ******************************************************
+
+    // Relación con la tabla 'users' (un empleado puede estar asociado a un usuario)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relación con la tabla 'companies' (un empleado está asociado a una empresa)
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    // Relación con la tabla 'offices' (un empleado está asociado a una oficina)
+    public function office()
+    {
+        return $this->belongsTo(Office::class);
+    }
+
+    // Relación con la tabla 'users' para el 'manager_id' (un empleado puede tener un jefe)
+    public function manager()
+    {
+        return $this->belongsTo(User::class, 'manager_id');
+    }
+}
---
+++ database/migrations/2025_04_10_135831_create_employees_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+
+            // Llave foránea
+            $table
+            ->foreignId('user_id')
+            ->nullable()
+            ->constrained('users', 'id')
+            ->onDelete('cascade')
+            ->onUpdate('cascade'); // Solo si tiene acceso al sistema
+
+            $table
+            ->foreignId('company_id')
+            ->constrained('companies', 'id')
+            ->onDelete('cascade')
+            ->onUpdate('cascade'); // Obligatorio
+
+            $table
+            ->foreignId('office_id')
+            ->constrained('offices', 'id')
+            ->onDelete('cascade')
+            ->onUpdate('cascade'); // Obligatorio
+
+            // Llave foránea
+            $table
+            ->foreignId('manager_id')
+            ->nullable()
+            ->constrained('users', 'id')
+            ->onDelete('cascade')
+            ->onUpdate('cascade'); // Jefe directo (opcional)
+
+            // Datos laborales
+            $table->string('employee_number')->unique(); // "OX-7890"
+            $table->string('job_title'); // "Ingeniero de software", "Diseñador"
+            $table->string('position'); // "Cajero", "Gerente de Sucursal"
+            $table->date('hired_at'); // Fecha_contratación
+            $table->enum('status', ['active', 'on_leave', 'fired']); // Estado
+            $table->decimal('salary', 10, 2)->nullable();
+            $table->string('shift'); // Turno laboral (Ej. "9 AM - 6 PM").
+            $table->string('emergency_contact');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
