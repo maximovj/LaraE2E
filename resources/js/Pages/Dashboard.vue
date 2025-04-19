b5fd4c7b91eaf790a02aff981886c5ebae8405e8
Víctor J. <maximovj@outlook.com>
2025-04-19 15:09:43 -0600

    feature: Se corrige condiciones

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,11 +5,10 @@ import { computed } from 'vue';
 import { usePage } from '@inertiajs/vue3';
 import { useAuthCache } from '@/composables/useAuthCache';
 
-const { hasRole, hasPermission } = useAuthCache();
+const { getCurrentRoles } = useAuthCache();
 
-const user = computed(() => usePage().props.auth.user);
-const currentRoles = computed(() => user.value?.roles || []);
-const currentPermissions = computed(() => user.value?.permissions || []);
+const canAccessSuperAdmin = computed(() => getCurrentRoles().includes(['super-admin']));
+
 </script>
 
 <template>
@@ -20,23 +19,13 @@ const currentPermissions = computed(() => user.value?.permissions || []);
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
-        <div v-if="hasRole('admin')"  class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-red-600 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-white">
-
-                        <p class="text-slate-700">You're logged in Admin!</p>
-                        <!-- Mostrar roles y permisos actuales -->
-                        <p class="text-white">Roles: {{ currentRoles.join(', ') }}</p>
-                        <p class="text-white">Permisos: {{ currentPermissions.join(', ') }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div v-else  class="py-12">
+        <div   class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div v-if="canAccessSuperAdmin" class="p-6 text-white">
+                        <p class="text-slate-700">You're logged in Admin!</p>
+                    </div>
+                    <div v-else class="p-6 text-gray-900">You're logged in!</div>
                 </div>
             </div>
         </div>
