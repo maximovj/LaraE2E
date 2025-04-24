ab5f426a83b1f2ed9369d70f3adad3180ca30829
Víctor J. <maximovj@outlook.com>
2025-04-24 14:01:21 -0600

    feature: Se crea un nuevo controlador para WorkActivity

---
+++ app/Http/Controllers/WorkActivityController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkActivity;
+use App\Models\WorkDay;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class WorkActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $employee = auth()->user()->load('employee')->employee;
+        $work_days = WorkDay::query()->where('employee_id', $employee->id)->with('events')->get();
+        $all_events = $work_days->pluck('events')->collapse();
+        return Inertia::render('WorkActivity/Index', [
+            'events' => $all_events->toArray(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WorkActivity $workActivity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WorkActivity $workActivity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, WorkActivity $workActivity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WorkActivity $workActivity)
+    {
+        //
+    }
+}
