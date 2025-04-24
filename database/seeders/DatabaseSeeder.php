88bb08a1361534615be05c63750ffc65582e9f86
Víctor J. <maximovj@outlook.com>
2025-04-24 15:19:19 -0600

    feature: Se modifica los seeder de WorkEvent

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,8 +33,8 @@ class DatabaseSeeder extends Seeder
             OfficesSeeder::class,
             EmployeesSeeder::class,
             WorkDaySeeder::class,
-            WorkEventSeeder::class,
             WorkActivitySeeder::class,
+            WorkEventSeeder::class,
         ]);
 
     }
--- database/seeders/WorkEventSeeder.php
+++ database/seeders/WorkEventSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Employee;
+use App\Models\WorkActivity;
 use App\Models\WorkDay;
 use App\Models\WorkEvent;
 use Illuminate\Database\Seeder;
@@ -19,19 +20,25 @@ class WorkEventSeeder extends Seeder
             $workDays = WorkDay::where('employee_id', $employee->id)->get();
 
             foreach ($workDays as $day) {
-                // Crear 1-3 eventos por día de trabajo
-                WorkEvent::factory()
-                    ->count(1)
-                    ->create([
-                        'employee_id' => $employee->id,
-                        'work_day_id' => $day->id,
-                    ]);
+                $workActivity = WorkActivity::where('work_day_id', $day->id)->first();
+                if($workActivity) {
+                    // Crear 1-3 eventos por día de trabajo
+                    WorkEvent::factory()
+                        ->count(1)
+                        ->create([
+                            'employee_id' => $employee->id,
+                            'work_day_id' => $day->id,
+                            'work_activity_id' => $workActivity->id,
+                        ]);
+                }
             }
         }
 
+        /*
         // Crear eventos aleatorios para otros empleados (opcional)
         WorkEvent::factory()
             ->count(50)
             ->create();
+        */
     }
 }
